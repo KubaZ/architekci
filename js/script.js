a3ff285256d38db7854139dcf94b576abdc0fcbe
KubaZ <Kuba@Jakub-Zmudzinskis-iMac.local>
2011-10-18 11:26:20 +0200

    added jReject

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 /* Author: 
 
 */
+$.reject({
+    reject: {
+        safari: false, // Apple Safari
+        chrome: false, // Google Chrome
+        firefox: false, // Mozilla Firefox
+        msie: true, // Microsoft Internet Explorer
+        opera: false, // Opera
+        konqueror: true, // Konqueror (Linux)
+        unknown: true // Everything else
+    }
+});
 $('#container').imagesLoaded( function(){
 	$(this).isotope({
   		masonry : {
